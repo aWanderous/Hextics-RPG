a014cb9298946c33e6dced0f04cf8ffe351d79d6
Alex Wan <a_wanderous@hotmail.com>
2020-12-16 12:29:31 +1100

    added style to statusbar and started developing app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import Tile from './components/Tile';
 class App extends Component {
     constructor()
     {
-       
+       super();
+       this.state = {
+           health: health,
+           mana: mana,
+       };
+    }
+
+    health= 100;
+    mana=100;
+
+    render() {
+        const {health,mana} = this.state;
+        return (
+            <div id='app'>
+                <StatusBar
+                health={health}
+                mana={mana}
+                />
+            </div>
+        );
     }
 }
+
+export default App;
