c8ab9914926cf421c2d983882840f387f9d68ac9
Alex Wan <a_wanderous@hotmail.com>
2020-12-17 12:34:29 +1100

    working app connected

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, {Component} from 'react';
 import StatusBar from './components/StatusBar';
-import Tile from './components/Tile';
+// import Tile from './components/Tile';
+import Footer from './components/Footer';
 
 class App extends Component {
     constructor()
     {
        super();
        this.state = {
-           health: health,
-           mana: mana,
+           health: 100,
+           mana: 100,
        };
     }
 
-    health= 100;
-    mana=100;
+
 
     render() {
         const {health,mana} = this.state;
         return (
             <div id='app'>
                 <StatusBar
-                health={health}
-                mana={mana}
+                health = {health}
+                mana = {mana}
                 />
+                <Footer />
             </div>
         );
     }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <span>By Alex Wan 2020 Reactjs</span>
+        </footer>
+    )
+}
+
+export default Footer;
